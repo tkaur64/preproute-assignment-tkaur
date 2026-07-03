2cb1635c295a44a1e4ab1d33fdd7d237cd616e3f
Tripat Kaur <tripatk1994@gmail.com>
2026-07-03 11:53:43 +0530

    feat: Implement routing with constants and add NotFound page

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  LOGIN: "/",
+  DASHBOARD: "/dashboard",
+  CREATE_TEST: "tests/create",
+  ADD_QUESTIONS: "/tests/:id/questions",
+  PREVIEW: "/tests/:id/preview",
+};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,25 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login/Login';
-import Dashboard from '../pages/Dashboard/Dashboard';
-import CreateTest from '../pages/CreateTest/CreateTest';
-import AddQuestions from '../pages/AddQuestions/AddQuestions';
-import PreviewAndPublish from '../pages/PreviewAndPublish/PreviewAndPublish';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { ROUTES } from "../constants/routes";
+
+import Login from "../pages/Login/Login";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import CreateTest from "../pages/CreateTest/CreateTest";
+import AddQuestions from "../pages/AddQuestions/AddQuestions";
+import PreviewAndPublish from "../pages/PreviewAndPublish/PreviewAndPublish";
+import NotFound from "../pages/NotFound/NotFound";
 
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create" element={<CreateTest />} />
-        {/* <Route path="/edit/:id" element={<EditTest />} /> */}
-        <Route path="/tests/:id/questions" element={<AddQuestions />} />
-        <Route path="/tests/:id/preview" element={<PreviewAndPublish />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+
+        <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+        <Route path={ROUTES.CREATE_TEST} element={<CreateTest />} />
+        <Route
+          path={ROUTES.ADD_QUESTIONS}
+
+          element={<AddQuestions />}
+        />
+        <Route
+          path={ROUTES.PREVIEW}
+          element={<PreviewAndPublish />}
+        />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default AppRoutes;
